3a89944f0e21cd36d1e24f633bb529431a3095a4
yu-kuguhara <you20010819@gmail.com>
2025-09-03 23:31:15 +0900

    タスクが完了した場合のチェックボックスを実装。

--- frontend/src/components/TodoItem.tsx
+++ frontend/src/components/TodoItem.tsx
@@ -1,8 +1,9 @@
 import type { Todo } from "@/types/todo";
 
-type Props = { todo: Todo };
+// 引数id=どのタスクを操作したかを特定するため、next=チェックの次の状態を伝える→該当タスクのcompletedを更新するため
+type Props = { todo: Todo; onToggle?: (id: number, next: boolean) => void }; //onToggle=「切替イベントが発生したら呼ばれる関数」というニュアンス
 
-export default function TodoItem({ todo }: Props) {
+export default function TodoItem({ todo, onToggle }: Props) {
   return (
     <li
       style={{
@@ -16,13 +17,23 @@ export default function TodoItem({ todo }: Props) {
         alignItems: "center",
       }}
     >
-      <div>
-        <div style={{ fontWeight: 600, marginBottom: 4 }}>
-          {todo.title} {todo.completed ? "✅" : ""}
-        </div>
-        <div style={{ fontSize: 12, color: "#6b7280" }}>
-          作成: {new Date(todo.createdAt).toLocaleString()}
-        </div>
+      <label
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: 8,
+          cursor: "pointer",
+        }}
+      >
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={(e) => onToggle?.(todo.id, e.currentTarget.checked)} //currentTarget=イベントが発生した要素(<input>要素)を指す(今回はcheckbox)
+        />
+        <span style={{ fontWeight: 600 }}>{todo.title}</span>
+      </label>
+      <div style={{ fontSize: 12, color: "#6b7280" }}>
+        作成: {new Date(todo.createdAt).toLocaleString()}
       </div>
     </li>
   );
